646ca1fb9b10f4a07bad98c308335d4b7476d549
Juan CB <juancb@usal.es>
2017-03-04 14:22:46 +0100

    First changes

--- gulpfile.js
+++ gulpfile.js
@@ -202,7 +202,7 @@ gulp.task('precache', function (callback) {
 gulp.task('gh-pages', function() {
   return gulp.src('./dist/**/*')
     .pipe(ghPages({
-      remoteUrl: 'https://github.com/snola/lasi16'
+      remoteUrl: 'https://github.com/snola/lasi17'
     }));
 });
 
